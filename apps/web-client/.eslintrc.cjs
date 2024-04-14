420942226bb16ee0ba8d2515c19189dfe09a4ddf
jinho222 <jinho020@yonsei.ac.kr>
2024-04-14 11:15:40 +0900

    update eslint rules

--- apps/web-client/.eslintrc.cjs
+++ apps/web-client/.eslintrc.cjs
@@ -1,18 +1,33 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   ignorePatterns: ['node_modules/', 'build/', 'dist/', 'coverage/'],
-  plugins: ['react', 'import'],
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:import/recommended', 'airbnb', 'airbnb/hooks', 'prettier'],
-  env: {
-    browser: true,
-    node: true,
-  },
-  settings: {
-    react: {
-      version: 'detect',
+  overrides: [
+    {
+      files: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.js', 'src/**/*.jsx'],
+      plugins: ['react'],
+      extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'airbnb', 'airbnb/hooks', 'prettier'],
+      rules: {
+        'import/no-named-as-default': 'off',
+        'import/no-named-as-default-member': 'off',
+        'import/no-unresolved': 'off',
+        'import/extensions': 'off',
+        'import/prefer-default-export': 'off',
+        'react/function-component-definition': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-filename-extension': 'off',
+        'react/prop-types': 'off',
+        'react/jsx-props-no-spreading': 'off',
+      },
+      parser: '@typescript-eslint/parser',
+      env: {
+        browser: true,
+        node: true,
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
     },
-  },
-  parserOptions: {
-    project: ['./tsconfig.json'],
-  },
+  ],
 };
