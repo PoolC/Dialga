27667a60b9347cf22c459e37c2adb75d55e721fc
jinoov <jinho020@yonsei.ac.kr>
2024-07-18 23:22:15 +0900

    fix: 알림버튼이 PC 버전에서는 나오지 않는 이슈

--- apps/web-client/src/components/header/Notification/Notification.tsx
+++ apps/web-client/src/components/header/Notification/Notification.tsx
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom';
 
 import { createStyles } from 'antd-style';
 import { NotificationControllerService, NotificationResponse, queryKey, useAppQuery } from '~/lib/api-v2';
+import { assert } from '~/lib/utils/assert';
 
 // CSS
 const useStyles = createStyles(() => ({
   dropdownMenu: {
     minHeight: '0px',
     maxHeight: '250px',
-    overflowY: 'scroll',
+    overflowY: 'auto',
   },
 
   dropdownItem: {
@@ -47,12 +48,17 @@ const convertDate = (inputDate: Date | string) => {
 // Components
 export default function Notification() {
   const { styles } = useStyles();
-  const { data }: { data?: NotificationResponse[] } = useAppQuery({
+  const { data } = useAppQuery({
     queryKey: queryKey.notification.unread,
-
-    queryFn: () => NotificationControllerService.getUnreadNotificationsUsingGet(),
-    placeholderData: [],
+    queryFn: async () => {
+      const res = await NotificationControllerService.getUnreadNotificationsUsingGet();
+      // NOTE: 204의 경우 아무것도 반환되지 않는다. 해당 케이스를 위한 처리
+      return res ?? [];
+    },
+    initialData: [],
   });
+
+  assert(data, 'data is undefined');
 
   const Menu = useCallback((menu: ReactNode) => <div className={styles.dropdownMenu}>{menu}</div>, [styles.dropdownMenu]);
 
@@ -89,24 +95,28 @@ export default function Notification() {
         return { link: '/', description: <p>오류가 발생했습니다</p> };
     }
   };
-  const dropDownItems = data?.map((dataOne) => ({
-    key: `${dataOne.createdAt}-${dataOne?.causedById ?? ''}-${dataOne.notificationType}`,
-    label: (
-      <Link to={resultLinkAndDescription(dataOne).link}>
-        <div>
-          <p>{convertDate(dataOne.createdAt ?? '')}</p>
-          <div className={styles.dropdownItem}>{resultLinkAndDescription(dataOne).description}</div>
-        </div>
-      </Link>
-    ),
-  }));
+
+  const dropDownItems =
+    data.length > 0
+      ? data.map((dataOne) => ({
+          key: `${dataOne.createdAt}-${dataOne?.causedById ?? ''}-${dataOne.notificationType}`,
+          label: (
+            <Link to={resultLinkAndDescription(dataOne).link}>
+              <div>
+                <p>{convertDate(dataOne.createdAt ?? '')}</p>
+                <div className={styles.dropdownItem}>{resultLinkAndDescription(dataOne).description}</div>
+              </div>
+            </Link>
+          ),
+        }))
+      : [{ key: 'empty', label: <p>새로운 알림이 없습니다.</p> }];
 
   return (
     <div>
       <Dropdown menu={{ items: dropDownItems }} dropdownRender={Menu}>
         <Button shape="circle" className={styles.dropdownButton}>
           <Space size="large" className={styles.dropdownShape}>
-            <Badge count={data?.length}>
+            <Badge count={data.length}>
               <Avatar shape="circle" size="default" icon={<BellOutlined />} className={styles.dropdownAvatar} />
             </Badge>
           </Space>
