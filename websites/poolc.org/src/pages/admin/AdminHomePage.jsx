07e62c05ed2a84be55a0ca8fe873d4ec9c79f0dd
Mayne0213 <bluemayne0213@icloud.com>
2026-08-26 19:23:18 +0900

    CHORE(admin-navigation): remove obsolete board tools
    - Remove unreachable board management routes and menu entry\n- Remove unused board admin components and API client

--- websites/poolc.org/src/pages/admin/AdminHomePage.jsx
+++ websites/poolc.org/src/pages/admin/AdminHomePage.jsx
@@ -4,8 +4,6 @@ import AdminBadgeContainer from '~/containers/admin/AdminBadgeContainer/AdminBad
 import AdminMenu from '../../components/admin/AdminMenu/AdminMenu';
 import { MENU } from '../../constants/menus';
 import AdminActivityContainer from '../../containers/admin/AdminActivityContainer/AdminActivityContainer';
-import AdminBoardContainer from '../../containers/admin/AdminBoardContainer/AdminBoardContainer';
-import AdminBoardFormContainer from '../../containers/admin/AdminBoardFormContainer/AdminBoardFormContainer';
 import AdminHomeContainer from '../../containers/admin/AdminHomeContainer/AdminHomeContainer';
 import AdminInfoContainer from '../../containers/admin/AdminInfoContainer/AdminInfoContainer';
 import AdminInterviewContainer from '../../containers/admin/AdminInterviewContainer/AdminInterviewContainer';
@@ -38,7 +36,6 @@ const AdminPage = ({ history }) => {
     { name: '동아리 정보 관리', url: '/info' },
     { name: '회원 관리', url: '/members' },
     { name: '최소활동기준 관리', url: '/pass' },
-    { name: '게시판 관리', url: '/boards' },
     { name: '프로젝트 관리', url: '/projects' },
     { name: '활동 관리', url: '/activities' },
     { name: '도서 관리', url: '/books' },
@@ -57,8 +54,6 @@ const AdminPage = ({ history }) => {
 
         <Route component={AdminMemberContainer} path={`/${MENU.ADMIN}/members`} exact />
         <Route component={AdminPassContainer} path={`/${MENU.ADMIN}/pass`} exact />
-        <Route component={AdminBoardFormContainer} path={[`/${MENU.ADMIN}/boards/new`, `/${MENU.ADMIN}/boards/edit/:boardID`]} exact />
-        <Route component={AdminBoardContainer} path={`/${MENU.ADMIN}/boards`} exact />
         <Route component={AdminProjectFormContainer} path={[`/${MENU.ADMIN}/projects/edit/:projectID`, `/${MENU.ADMIN}/projects/new`]} exact />
         <Route component={AdminProjectContainer} path={`/${MENU.ADMIN}/projects`} exact />
         <Route component={AdminActivityContainer} path={`/${MENU.ADMIN}/activities`} exact />
